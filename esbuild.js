9c4841fe8fdb72b9a0e5b9f1f2cd2a57d3cc3eb0
Tim Niederer <niederer.tim@gmail.com>
2023-02-23 00:43:09 +0100

    fix: generate ts definition files for publishing

--- esbuild.js
+++ esbuild.js
@@ -1,11 +1,19 @@
 const esbuild = require("esbuild");
 const packageJson = require("./package.json");
+const fs = require("fs");
 
-esbuild.build({
-    entryPoints: ["./src/index.ts"],
-    minify: true,
-    bundle: true,
-    outfile: "./dist/index.js",
-    platform: "node",
-    external: Object.keys(packageJson.dependencies),
-});
+// Delete the dist folder
+const deleteDist = () => fs.rmSync("dist", { force: true, recursive: true });
+deleteDist();
+
+// Bundle the cli
+esbuild
+    .build({
+        entryPoints: ["./src/index.ts"],
+        minify: true,
+        bundle: true,
+        outfile: "./dist/index.js",
+        platform: "node",
+        external: Object.keys(packageJson.dependencies),
+    })
+    .catch(deleteDist);
