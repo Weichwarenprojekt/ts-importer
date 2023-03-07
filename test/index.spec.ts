7b65895de2a03dda92f1f21eee3937da98d9b723
Tim Niederer <niederer.tim@gmail.com>
2023-03-07 01:12:48 +0100

    test: use commonjs file for testing exports

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it } from "@jest/globals";
 
 describe("Index E2E", () => {
-    it("exports all members", () => {
-        const exports = require("../dist/index.js");
+    it("exports all members (commonjs)", () => {
+        const exports = require("../dist/index.cjs");
         expect(Object.keys(exports).length).toEqual(1);
         expect(exports.loadModule).toEqual(expect.any(Function));
     });
